5c021c22274ee21dcb481c0ca23f6ebcaeb2b589
Manikanta Naidu <manikantanaidu4880@outlook.com>
2025-03-27 20:10:01 +0530

    Added Interface for best coding practise

---
+++ JWTAuthDotNet9/Services/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using JWTAuthDotNet9.Entities;
+using JWTAuthDotNet9.Model;
+
+namespace JWTAuthDotNet9.Services
+{
+    public interface IAuthService
+    {
+        Task<User?> RegisterAsync(UserDto request);
+        Task<string?> LoginAsync(UserDto request);
+    }
+}
