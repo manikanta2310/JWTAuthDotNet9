b9b3a858d6f50bf3c75d03473731aa1c548c8852
Manikanta Naidu <manikantanaidu4880@outlook.com>
2025-03-27 18:33:26 +0530

    Added Configurationn and Token and Issuer ,Audience in appsetting and tested the file in JWT.IO

--- JWTAuthDotNet9/Controllers/AuthController.cs
+++ JWTAuthDotNet9/Controllers/AuthController.cs
@@ -2,13 +2,17 @@
 using JWTAuthDotNet9.Model;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace JWTAuthDotNet9.Controllers
 {
 
     [Route("api/[Controller]")]
     [ApiController]
-    public class AuthController : Controller
+    public class AuthController(IConfiguration Configuration) : Controller
     {
         public static User user = new();
 
@@ -34,8 +38,35 @@ namespace JWTAuthDotNet9.Controllers
                 return BadRequest("Invalid password");
             }
 
-            string token = "Success";
+            string token = Createtoken(user);
             return Ok(token);
         }
+
+        // Generating JWT token using claims 
+        private string Createtoken(User user)
+        {
+            var claims = new List<Claim>
+            {
+                // name identifier usuallly is used for the actual ID of the user
+                new Claim(ClaimTypes.Name, user.Username)
+            };
+            // need to add a secret key to sign the token
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration.GetValue<string>("AppSettings:Token")!));
+
+            // need to add the key to the signing credentials
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512);
+
+            var tokenDescription = new JwtSecurityToken(
+                issuer: Configuration.GetValue<string>("AppSettings:Issuer"),
+                audience: Configuration.GetValue<string>("AppSettings:Audience"),
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: creds
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(tokenDescription);
+        }
+
+
     }
 }
